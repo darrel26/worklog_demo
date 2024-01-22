45a06efb79ab190026c130e06ced62938a23b604
darrel26 <dionisiusgunadi@outlook.com>
2024-01-23 00:40:41 +0700

    add route to get specific user and user details

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using worklog_demo.Data;
 using worklog_demo.Models;
+using worklog_demo.Models.DTO.Responses;
 
 namespace worklog_demo.Controllers
 {
@@ -24,10 +21,10 @@ namespace worklog_demo.Controllers
         }
 
         [HttpGet]
-        [ProducesResponseType(200, Type = typeof(TbUser))]
+        [ProducesResponseType(200, Type = typeof(UsersResponse))]
         [SwaggerResponse(204, "No Content")]
         [ProducesResponseType(400)]
-        public ActionResult<IEnumerable<TbUser>> GetUsersItem()
+        public ActionResult<IEnumerable<UsersResponse>> GetUsersItem()
         {
             _context = HttpContext.RequestServices.GetService(typeof(UserContext)) as UserContext;
             var data = _context.GetAllUsers();
@@ -45,5 +42,46 @@ namespace worklog_demo.Controllers
             return Ok(data);
         }
 
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(UsersResponse))]
+        [SwaggerResponse(404, "Not Found")]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<UsersResponse>> GetSpecificUser(int id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(UserContext)) as UserContext;
+            var user = _context.GetSpecificUser(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("details/{id}")]
+        [ProducesResponseType(200, Type = typeof(UserDetailResponse))]
+        [SwaggerResponse(404, "Not Found")]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<UserDetailResponse>> GetUserDetail(int id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(UserContext)) as UserContext;
+            var user = _context.GetUserDetail(id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(user);
+        }
     }
 }
