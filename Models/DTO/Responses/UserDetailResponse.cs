4f0f722486a0de5336e368697162bfad65078731
darrel26 <dionisiusgunadi@outlook.com>
2024-01-23 00:40:08 +0700

    Add DTO responses models

---
+++ Models/DTO/Responses/UserDetailResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace worklog_demo.Models.DTO.Responses
+{
+    public class UserDetailResponse : UsersResponse
+    {
+        public UserDetailResponse()
+        {
+            Worklogs = new HashSet<TbWorklog>();
+        }
+
+        public virtual ICollection<TbWorklog> Worklogs { get; set; }
+    }
+}
